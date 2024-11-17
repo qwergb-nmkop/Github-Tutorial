2a00bce1ccf17f735624bdc8c56e133485b8f2d4
Majd <91481525+qwergb-nmkop@users.noreply.github.com>
2024-11-17 17:18:37 +0400

    added a new multiply function
    in this update I added a new function called (multiply) that can calculate the multiplication value of any two numbers

--- print(hello world).py
+++ print(hello world).py
@@ -2,3 +2,9 @@ print("hello world")
 print("it's cool that visual studio code can directly upload to github")
 for i in range(10):
     print("high",i)
+
+def multiply(x,y):
+    print(x,"times", y, "equals:",x*y)
+
+
+multiply(5,3)
